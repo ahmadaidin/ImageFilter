2ed511563f49b5c1bac59cb61943e366f85f308a
ahmadaidin <ahmadaidin08.aa@gmail.com>
2017-02-22 14:04:59 +0700

    tambah FFT

---
+++ app/src/main/java/com/example/ahmadaidin/imgfilter/FrequencyDomain/Complex.java
@@ -0,0 +1,8 @@
+package com.example.ahmadaidin.imgfilter.FrequencyDomain;
+
+/**
+ * Created by Aidin Ahmad on 15/11/2016.
+ */
+
+public class Complex {
+}
---
+++ app/src/main/java/com/example/ahmadaidin/imgfilter/FrequencyDomain/FrequencyFilter.java
@@ -0,0 +1,9 @@
+package com.example.ahmadaidin.imgfilter.FrequencyDomain;
+
+/**
+ * Created by Aidin Ahmad on 15/11/2016.
+ */
+
+public class FrequencyFilter {
+
+}
